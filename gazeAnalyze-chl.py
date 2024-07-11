8674ede3b10868107a3e00f4054e0dd8ff41deeb
rladjwls57 <130573218+rladjwls57@users.noreply.github.com>
2024-07-11 13:50:21 +0900

    Update gazeAnalyze-chl.py

--- gazeAnalyze-chl.py
+++ gazeAnalyze-chl.py
@@ -7,7 +7,7 @@ def load_data(files):
     gaze_x = []
     gaze_y = []
 
-    for file in files:
+    for file in files:4
         with open(file, 'r') as f:
             data = json.load(f)
             for key, value in data.items():
@@ -19,17 +19,16 @@ def load_data(files):
 fig, axs = plt.subplots(3, 5, figsize=(15, 15))  # 3x5 그리드의 서브플롯
 fig.suptitle('Visualization for Each Frame')
 
+files = [f'{i:02d}.json' for i in range(15)]  
 
-
-
-for file in files: #한 figure에 파일 00~14까지 색상 다르개 해서 축 올림
+i = 0  # 인덱스 초기화
+for file in files:  # 한 figure에 파일 00~14까지 색상 다르게 해서 축 올림
     if not os.path.exists(file):
         print(f"{file} 불러오기 오류")
         continue
-    i=0
-    files = [f'{i:02d}.json']  
+
     row = i // 5
-    col = i % 3
+    col = i % 5
     ax = axs[row, col]
     
     gaze_x, gaze_y = load_data([file])
@@ -41,16 +40,14 @@ for file in files: #한 figure에 파일 00~14까지 색상 다르개 해서 축
     for j in range(len(gaze_x)):
         ax.text(gaze_x[j], gaze_y[j], f'{j}', fontsize=8, ha='right')
     
-    ax.set_title(\d i".json")
+    ax.set_title(file)
     ax.set_xlabel("gaze_x")
     ax.set_ylabel("gaze_y")
     ax.grid(True)
-    i++
+    
+    i += 1  # 인덱스 증가
 
-plt.title('Gaze Coordinates')
-#plt.xlabel('X Coordinate')
-#plt.ylabel('Y Coordinate')
-#plt.legend()
 plt.tight_layout(rect=[0, 0, 1, 0.96])
 
 plt.show()
+
